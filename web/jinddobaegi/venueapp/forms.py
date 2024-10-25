6ebb1b091156d0b94d3e69e87da5d7c2037d03d8
dkswhale <dkswhale@gmail.com>
2024-10-25 13:04:01 +0900

    ??

--- web/jinddobaegi/venueapp/forms.py
+++ web/jinddobaegi/venueapp/forms.py
@@ -1,50 +1,18 @@
 from django import forms
 #
-# from venueapp.models import UserVisit
+from .models import UserVisit
 
 
-# class VenueRatingForm(forms.ModelForm):
-#     class Meta:
-#         model = UserVisit
-#         fields = ['score', 'saltiness', 'sourness', 'spiciness', 'sweetness', 'cleanliness']
-#         widgets = {
-#             'score': forms.Select(choices=[(i, i) for i in range(6)]),
-#             'saltiness': forms.Select(choices=[(i, i) for i in range(-5, 6)]),
-#             'sourness': forms.Select(choices=[(i, i) for i in range(-5, 6)]),
-#             'spiciness': forms.Select(choices=[(i, i) for i in range(-5, 6)]),
-#             'sweetness': forms.Select(choices=[(i, i) for i in range(-5, 6)]),
-#             'cleanliness': forms.Select(choices=[(i, i) for i in range(-5, 6)]),
-#         }
-
-class VenueRatingForm(forms.Form):
-    score = forms.ChoiceField(
-        choices=[(i, i) for i in range(6)],
-        label='Score',
-        widget=forms.Select
-    )
-    flavor1 = forms.ChoiceField(
-        choices=[(i, i) for i in range(-5, 6)],
-        label='Flavor 1',
-        widget=forms.Select
-    )
-    flavor2 = forms.ChoiceField(
-        choices=[(i, i) for i in range(-5, 6)],
-        label='Flavor 2',
-        widget=forms.Select
-    )
-    flavor3 = forms.ChoiceField(
-        choices=[(i, i) for i in range(-5, 6)],
-        label='Flavor 3',
-        widget=forms.Select
-    )
-    flavor4 = forms.ChoiceField(
-        choices=[(i, i) for i in range(-5, 6)],
-        label='Flavor 4',
-        widget=forms.Select
-    )
-    flavor5 = forms.ChoiceField(
-        choices=[(i, i) for i in range(-5, 6)],
-        label='Flavor 5',
-        widget=forms.Select
-    )
+class VenueRatingForm(forms.ModelForm):
+    class Meta:
+        model = UserVisit
+        fields = ['score', 'saltiness', 'sourness', 'spiciness', 'sweetness', 'cleanliness']
+        widgets = {
+            'score': forms.Select(choices=[(i, i) for i in range(6)]),
+            'saltiness': forms.Select(choices=[(i, i) for i in range(-5, 6)]),
+            'sourness': forms.Select(choices=[(i, i) for i in range(-5, 6)]),
+            'spiciness': forms.Select(choices=[(i, i) for i in range(-5, 6)]),
+            'sweetness': forms.Select(choices=[(i, i) for i in range(-5, 6)]),
+            'cleanliness': forms.Select(choices=[(i, i) for i in range(-5, 6)]),
+        }
 
